e3452d5967336b253fd5310dad372a4a74effe2f
Polomaru <paul.rios@utec.edu.pe>
2021-06-30 23:19:07 -0500

    tweets

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # BD2_P2
+
+Segunda parte proyecto base de datos 2.
+
+## Autors
+
+* [Paul Rios](https://github.com/Polomaru)
+* [Efraín Córdova](https://github.com/ecordovaa)
+* [David Soto](https://github.com/vid58)
+
+### Built With
+
+* python 3.8 (LINUX)
+* pip
+* flask(?)
+* [snscrape] (https://github.com/JustAnotherArchivist/snscrape) 
+
+### Prerequisites
+
+We need snscrape.
+* flex
+  ```sh
+  sudo apt-get update
+  pip install --upgrade git+https://github.com/JustAnotherArchivist/snscrape
+  ```
--- tweets.py
+++ tweets.py
@@ -1,31 +1,4 @@
-import tweepy
-import csv
-import pandas as pd
-import GetOldTweets3 as got
-import time
-
-credentials_df = pd.read_csv('credentials.csv',header=None,names=['name','key'])
-
-consumer_key = credentials_df.loc[credentials_df['name']=='consumer_key','key'].iloc[0]
-consumer_secret = credentials_df.loc[credentials_df['name']=='consumer_secret','key'].iloc[0]
-access_token = credentials_df.loc[credentials_df['name']=='access_token','key'].iloc[0]
-access_token_secret = credentials_df.loc[credentials_df['name']=='access_secret','key'].iloc[0]
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth,wait_on_rate_limit=True)
-
-HashValue = input("Enter the hashtag you want the tweets to be downloaded for: ")
-
-# Open/Create a file to append data
-csvFile = open('result.csv', 'a')
-
-#Use csv Writer
-csvWriter = csv.writer(csvFile)
-
-for tweet in tweepy.Cursor(api.search,q=HashValue).items():
-    print (tweet.created_at, tweet.full_text)
-    csvWriter.writerow([tweet.created_at, tweet.full_text.encode('utf-8')])
-
-print ("Scraping finished and saved to "+HashValue+".csv")
-#sys.exit()
+import os 
+import json
+io = input("Ingrese query: ")
+os.system('snscrape --jsonl --max-results 200  twitter-search " ' + io + ' until:2021-06-30 lang:es "  > myjson.json') 
