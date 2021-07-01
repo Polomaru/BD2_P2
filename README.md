a84fc40613a68ef46eb5454bdeff642d914f5890
Polomaru <paul.rios@utec.edu.pe>
2021-07-01 00:46:17 -0500

    fix

--- README.md
+++ README.md
@@ -32,3 +32,13 @@ We need snscrape.
 ```
 
 Then this will get all 200 first tweets with the word "efra" until 30-06-21.
+
+### Reference
+
+* https://medium.datadriveninvestor.com/how-to-build-a-twitter-scraping-app-with-python-b3fc069a19c6
+
+How to use snscrape.
+
+* https://github.com/JustAnotherArchivist/snscrape
+
+Use of snscrape
--- tweets.py
+++ tweets.py
@@ -1,4 +1,31 @@
-import os 
+import csv
+import snscrape.modules.twitter as sntwitter
 import json
-io = input("Ingrese query: ")
-os.system('snscrape --jsonl --max-results 200  twitter-search " ' + io + ' until:2021-06-30 lang:es "  > myjson.json') 
+
+keyword = input('Enter a topic or keyword, please: ')
+
+maxTweets = 100
+
+# csvFile = open('search.csv', 'a', newline='', encoding='utf8')
+
+# csvWriter = csv.writer(csvFile)
+# csvWriter.writerow(['n','id','username','date','content','url'])
+
+
+json_file = open('data.json', 'a', newline='\n', encoding='utf8')
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper(keyword + ' lang:es until:2021-06-30 -filter:links -filter:replies').get_items())  :
+        if i > maxTweets :
+            break
+        my_details = {
+            'n': i,
+            'id': tweet.id,
+            'username' : tweet.username,
+            'date' : tweet.date,
+            'content' : tweet.content,
+            'url' : tweet.url
+        }
+        json.dump(my_details, json_file, default=str, ensure_ascii=False, separators=(',', ':'))
+        json_file.write('\n')
+
+        # csvWriter.writerow([i,tweet.id, tweet.username ,tweet.date, tweet.content, tweet.url])
+# csvFile.close()
